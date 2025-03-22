e0ebc5f604c36ed7ad70132a1f92360c273afa0f
CommanderRedYT <ccommanderred@gmail.com>
2025-03-22 19:23:08 +0100

    Brick everything because of bullshit arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <Arduino.h>
 
+#define VARIANT_HTTP 1
+#define VARIANT_MQTT 2
+
+#define VARIANT VARIANT_MQTT
+
+#if VARIANT == VARIANT_HTTP
+#define USE_HTTP
+#elif VARIANT == VARIANT_MQTT
+#define USE_MQTT
+#endif
+
 // libs
 #include <ArduinoJson.h>
+#ifdef USE_HTTP
 #include <HTTPClient.h>
+#endif
+#ifdef USE_MQTT
+#include <PubSubClient.h>
+#endif
 #include <WiFi.h>
 #include <array>
 #include <optional>
@@ -10,10 +26,23 @@
 
 #define DEBUG
 
+#ifdef USE_HTTP
 HTTPClient http;
 
-int retry = 0;
 uint64_t lastTime = 0;
+#endif
+
+#ifdef USE_MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define MQTT_TOPIC "realraum/sperrzu/online"
+#define ONLINE_PAYLOAD "{\"online\":true}"
+#define OFFLINE_PAYLOAD "{\"online\":false}"
+#define MQTT_SERVER "mqtt.realraum.at"
+#endif
+
+int retry = 0;
 
 std::optional<uint64_t> millisBlinkingStarted = 0;
 
@@ -23,7 +52,8 @@ bool backDoorLocked = false;
 bool frontDoorLocked = false;
 bool w2Locked = false;
 
-bool w1ajar = false;
+bool w1frontAjar = false;
+bool w1backAjar = false;
 bool w2ajar = false;
 
 #define LED_PIN 5
@@ -35,6 +65,61 @@ bool w2ajar = false;
 #define HINTEN_INDEX 1
 
 std::array<CRGB, NUM_LEDS> dingsbums;
+
+#ifdef USE_MQTT
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  // json parse
+  String payloadStr;
+
+  for (int i = 0; i < length; i++)
+  {
+    payloadStr += (char)payload[i];
+  }
+
+  Serial.printf("Message arrived [%s] %s\n", topic, payloadStr.c_str());
+
+  doc.clear();
+
+  if (const auto error = deserializeJson(doc, payloadStr); error)
+  {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  // ==== MQTT ====
+  // backDoorLocked: topic: "realraum/backdoorcx/lock", from payload: x.Lock
+  // frontDoorLocked: topic: "realraum/frontdoor/lock", from payload: x.Lock
+  // w2Locked: topic: "realraum/w2frontdoor/lock", from payload: x.Lock
+
+  // w1ajar-0: topic: "realraum/frontdoor/ajar", from payload: x.Shut
+  // w1ajar-1: topic: "realraum/backdoorcx/ajar", from payload: x.Shut
+  // w2ajar-0: topic: "realraum/w2frontdoor/ajar", from payload: x.Shut
+  
+  String topicStr = topic;
+
+  #define TOPIC_CASE(match, expr) \
+    if (strcmp(topic, match) == 0) { \
+      Serial.printf("Matched topic: %s\n", match); \
+      expr; \
+      return; \
+    }
+
+  TOPIC_CASE("realraum/backdoorcx/lock", backDoorLocked = doc["Lock"]);
+  TOPIC_CASE("realraum/frontdoor/lock", frontDoorLocked = doc["Lock"]);
+  TOPIC_CASE("realraum/w2frontdoor/lock", w2Locked = doc["Lock"]);
+  TOPIC_CASE("realraum/frontdoor/ajar", w1frontAjar = doc["Shut"]);
+  TOPIC_CASE("realraum/backdoorcx/ajar", w1backAjar = doc["Shut"]);
+  TOPIC_CASE("realraum/w2frontdoor/ajar", w2ajar = doc["Shut"]);
+
+  #undef TOPIC_CASE
+
+  Serial.printf("Unknown topic: %s\n", topic);
+}
+
+#endif
 
 void setDingsbums(int dingsbumsIndex, CRGB color)
 {
@@ -58,10 +143,13 @@ void setup()
 
   Serial.begin(115200);
 
+  while (!Serial)
+    ;
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   WiFi.setHostname("dingsbums");
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
+  WiFi.setAutoReconnect(false);
+  WiFi.persistent(false);
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -72,6 +160,11 @@ void setup()
   Serial.println("Connected to the WiFi network");
 
   FastLED.setBrightness(255);
+
+#ifdef USE_MQTT
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
+#endif
 }
 
 void loop()
@@ -89,7 +182,12 @@ void loop()
       return;
     }
 
-    Serial.println("Connecting to WiFi...");
+    // get err str
+    wl_status_t err = WiFi.status();
+
+    Serial.printf("WiFi not connected, status: %d\n", err);
+
+    Serial.printf("Connecting to WiFi \"%s\"...\n", WIFI_SSID);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     // wait 5 seconds
@@ -97,6 +195,37 @@ void loop()
     retry++;
   }
 
+#ifdef USE_MQTT
+  if (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+
+    while (!client.connected())
+    {
+      std::fill(dingsbums.begin(), dingsbums.end(), CRGB::White);
+      FastLED.show();
+
+      Serial.println("Connecting to MQTT...");
+
+      if (client.connect("dingsbums", MQTT_TOPIC, 0, true, OFFLINE_PAYLOAD))
+      {
+        Serial.println("Connected to MQTT");
+
+        client.subscribe("realraum/#");
+
+        client.publish(MQTT_TOPIC, ONLINE_PAYLOAD);
+      }
+      else
+      {
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        delay(2000);
+      }
+    }
+  }
+#endif
+
+#ifdef USE_HTTP
   if (lastTime == 0 || millis() - lastTime > 1000)
   {
     http.begin("http://realraum.at/status.json");
@@ -107,6 +236,8 @@ void loop()
 
     if (httpCode > 0)
     {
+      doc.clear();
+
       if (const auto error = deserializeJson(doc, http.getString()); error)
       {
         Serial.print("deserializeJson() failed: ");
@@ -128,7 +259,8 @@ void loop()
       w2ajar = !doc["sensors"]["ext_door_ajar"][0]["value"];
 
       // w1 ajar = !x.sensors.ext_door_ajar[4].value || !x.sensors.ext_door_ajar[5].value
-      w1ajar = !doc["sensors"]["ext_door_ajar"][4]["value"] || !doc["sensors"]["ext_door_ajar"][5]["value"];
+      w1frontAjar = !doc["sensors"]["ext_door_ajar"][4]["value"];
+      w1backAjar = !doc["sensors"]["ext_door_ajar"][5]["value"];
     }
     else
     {
@@ -144,6 +276,7 @@ void loop()
     Serial.printf("W2 ajar: %s\n", w2ajar ? "yes" : "no");
 #endif
   }
+#endif
 
   bool stayOn = millisBlinkingStarted && millis() - millisBlinkingStarted.value() > 5000;
 
@@ -163,7 +296,7 @@ void loop()
     setDingsbums(HINTEN_INDEX, CRGB::Green);
   }
 
-  if (w1ajar) {
+  if (w1frontAjar || w1backAjar) {
     setDingsbums(VORNE_INDEX, blink ? CRGB::Blue : CRGB::Black);
     irgendwasBlinktlol = true;
   } else if (frontDoorLocked && !backDoorLocked) {
